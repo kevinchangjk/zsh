6d776657af1390d2a6217ea4e1d795a1cacb00dd
NguyenVuKhang <brew4k@gmail.com>
2021-05-24 23:56:55 +0800

    added quote script

---
+++ quote/quotes.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+import json
+import textwrap
+import subprocess
+import random
+import os
+
+f = open('repos/zsh/quote/quotes.json','r')
+data = json.load(f)
+tput = str(subprocess.check_output(['tput','cols']))
+width = int(tput.split("\'")[1].split("\\")[0]) - 4
+c = random.randrange(1596)
+
+wrapped = textwrap.wrap(data[c]['text'],width)
+wrapped = ["  "+x for x in wrapped]
+print(*wrapped,sep="\n")
+print()
+print("    -",data[c]['author'])
+print()
+
+f.close()
