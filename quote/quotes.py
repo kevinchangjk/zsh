31614752ee3c819e51641d6b5e0d3e70b79f9055
nguyenvukhang <brew4k@gmail.com>
2021-10-13 08:42:51 +0800

    made zsh open quotes green

--- quote/quotes.py
+++ quote/quotes.py
@@ -4,6 +4,17 @@ import textwrap
 import subprocess
 import random
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 f = open('/home/khang/repos/zsh/quote/quotes.json','r')
 data = json.load(f)
 f.close()
@@ -13,7 +24,9 @@ c = random.randrange(1596)
 
 wrapped = textwrap.wrap(data[c]['text'],width)
 wrapped = ["  "+x for x in wrapped]
+print(bcolors.OKGREEN,end='')
 print(*wrapped,sep="\n")
+print(bcolors.ENDC,end='')
 print()
 print("    -",data[c]['author'])
 print()
